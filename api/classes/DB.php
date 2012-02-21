3c845deae03fd95afd701916ab1186ca7b58b828
Bryan Mishkin <bryan.mishkin@gmail.com>
2012-02-21 01:50:35 -0600

    Fixing database selection

--- api/classes/DB.php
+++ api/classes/DB.php
@@ -1,14 +1,14 @@
 <?php
 class DB {
 	public static function connect() {
-		global $con,$dbHost,$dbUser,$dbPass;
+		global $con,$dbHost,$dbUser,$dbPass,$dbName;
 		$con = mysql_connect($dbHost,$dbUser,$dbPass);
 		if (!$con) {
 			echo 'Could not connect: '.mysql_error();
 		}
 		
-		$db_selected = mysql_select_db($dbName, $con);
-		if (!$db_selected) {
+		$dbSelected = mysql_select_db($dbName, $con);
+		if (!$dbSelected) {
 		    echo 'Can\'t select database: '.mysql_error();
 		}
 	}
