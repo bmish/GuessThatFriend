182bcce07569ecd9a37d72bdc3b0f30b25f9a764
Tianyi Wang <wang103@illinois.edu>
2012-02-22 00:42:01 -0600

    implemented the result view

--- iphone/GuessThatFriend/Classes/MultipleChoiceQuizViewController.h
+++ iphone/GuessThatFriend/Classes/MultipleChoiceQuizViewController.h
@@ -9,16 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "QuizBaseViewController.h"
 
+@class QuizFeedbackViewController;
+
 @interface MultipleChoiceQuizViewController : QuizBaseViewController <UITableViewDataSource, UITableViewDelegate> {	
-	UITableView *friendsTable;
+    
+    UITableView *friendsTable;
 	
-	NSString *questionString;
-	NSMutableArray *friendsList;
+    NSString *questionString;
+    NSMutableArray *friendsList;
+    
+    QuizFeedbackViewController *quizFeedbackViewController;
 }
 
 @property (nonatomic, retain) IBOutlet UITableView *friendsTable;
-
 @property (nonatomic, retain) NSString *questionString;
 @property (nonatomic, retain) NSArray *friendsList;
+@property (nonatomic, retain) QuizFeedbackViewController *quizFeedbackViewController;
 
 @end
