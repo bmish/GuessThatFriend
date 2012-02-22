1d1a81152fe538776eb3f08142771c518bd41b93
Tianyi Wang <wang103@illinois.edu>
2012-02-22 04:27:33 -0600

    UI for quiz setting is done.

--- iphone/GuessThatFriend/Classes/SettingsViewController.h
+++ iphone/GuessThatFriend/Classes/SettingsViewController.h
@@ -9,14 +9,34 @@
 #import <UIKit/UIKit.h>
 
 @interface SettingsViewController : UIViewController {
+    UISlider *noQuestionsSlider;
+    UILabel *noQuestionsLabel;
+    UISegmentedControl *quizOptionSegmentedControl;
+    UILabel *quizOptionLabel;
+    UIButton *targetFriendEditButton;
+    UILabel *targetFriendLabel;
+    UIButton *categoryEditButton;
+    UILabel *categoryLabel;
+    
 	UIButton *logoutButton;
 	UIButton *backButton;
 }
+
+@property (nonatomic, retain) IBOutlet UISlider *noQuestionsSlider;
+@property (nonatomic, retain) IBOutlet UILabel *noQuestionsLabel;
+@property (nonatomic, retain) IBOutlet UISegmentedControl *quizOptionSegmentedControl;
+@property (nonatomic, retain) IBOutlet UILabel *quizOptionLabel;
+@property (nonatomic, retain) IBOutlet UIButton *targetFriendEditButton;
+@property (nonatomic, retain) IBOutlet UILabel *targetFriendLabel;
+@property (nonatomic, retain) IBOutlet UIButton *categoryEditButton;
+@property (nonatomic, retain) IBOutlet UILabel *categoryLabel;
 
 @property (nonatomic, retain) IBOutlet UIButton *logoutButton;
 @property (nonatomic, retain) IBOutlet UIButton *backButton;
 
 - (IBAction)switchViewToFBLogin:(id)sender;
 - (IBAction)switchViewToGoBack:(id)sender;
+- (IBAction)noQuestionSliderChanged:(id)sender;
+- (IBAction)quizOptionSegmentedControlChanged:(id)sender;
 
 @end
--- iphone/GuessThatFriend/Classes/SettingsViewController.m
+++ iphone/GuessThatFriend/Classes/SettingsViewController.m
@@ -10,6 +10,14 @@
 
 @implementation SettingsViewController
 
+@synthesize noQuestionsSlider;
+@synthesize noQuestionsLabel;
+@synthesize quizOptionSegmentedControl;
+@synthesize quizOptionLabel;
+@synthesize targetFriendEditButton;
+@synthesize targetFriendLabel;
+@synthesize categoryEditButton;
+@synthesize categoryLabel;
 @synthesize logoutButton;
 @synthesize backButton;
 
@@ -30,13 +38,53 @@
 	UIImage *stretchableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 topCapHeight:0];
 	[logoutButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
 	[backButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
-	
+	[targetFriendEditButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+	[categoryEditButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    
 	UIImage *buttonImagePressed = [UIImage imageNamed:@"blueButton.png"];
 	UIImage *stretchableButtonImagePressed = [buttonImagePressed stretchableImageWithLeftCapWidth:12 topCapHeight:0];
 	[logoutButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
 	[backButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
-	
+	[targetFriendEditButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
+	[categoryEditButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
+    
     [super viewDidLoad];
+}
+
+- (IBAction)noQuestionSliderChanged:(id)sender {
+    int value = (int)noQuestionsSlider.value;
+    noQuestionsLabel.text = [NSString stringWithFormat:@"%d", value];
+}
+
+- (IBAction)quizOptionSegmentedControlChanged:(id)sender {
+    int selected = quizOptionSegmentedControl.selectedSegmentIndex;
+    NSString *option;
+    switch (selected) {
+        case 0:
+            option = [NSString stringWithString:@"Random"];
+            break;
+        case 1:
+            option = [NSString stringWithString:@"Fill in blanks"];
+            break;
+        case 2:
+            option = [NSString stringWithString:@"2 choices"];
+            break;
+        case 3:
+            option = [NSString stringWithString:@"3 choices"];
+            break;
+        case 4:
+            option = [NSString stringWithString:@"4 choices"];
+            break;
+        case 5:
+            option = [NSString stringWithString:@"5 choices"];
+            break;
+        case 6:
+            option = [NSString stringWithString:@"6 choices"];
+            break;
+        default:
+            break;
+    }
+    quizOptionLabel.text = option;
 }
 
 /*
@@ -86,16 +134,37 @@
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 	
+    self.noQuestionsSlider = nil;
+    self.noQuestionsLabel = nil;
+    self.quizOptionSegmentedControl = nil;
+    self.quizOptionLabel = nil;
+    self.targetFriendEditButton = nil;
+    self.targetFriendLabel = nil;
+    self.categoryEditButton = nil;
+    self.categoryLabel = nil;
+    self.logoutButton = nil;
+    self.backButton = nil;
+    
 	self.logoutButton = nil;
 	self.backButton = nil;
 }
 
 - (void)dealloc {
+    [noQuestionsSlider release];
+    [noQuestionsLabel release];
+    [quizOptionSegmentedControl release];
+    [quizOptionLabel release];
+    [targetFriendEditButton release];
+    [targetFriendLabel release];
+    [categoryEditButton release];
+    [categoryLabel release];
+    [logoutButton release];
+    [backButton release];
+    
 	[logoutButton release];
 	[backButton release];
 	
     [super dealloc];
 }
-
 
 @end
