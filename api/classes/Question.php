efd869955e3dd3366a362fb9daf1ada3629489c8
mulliki1 <legoman90@gmail.com>
2012-03-05 22:17:23 -0600

    Moved optionID fields to MCQuestion.php

--- api/classes/Question.php
+++ api/classes/Question.php
@@ -4,9 +4,10 @@ class Question
 	protected $questionId;
 	protected $categoryId;		// Category of this Question (like books or movies).
 	protected $subjectId;		// Subject of this Question (a person or page).
-	protected $text;			// Question text.
-	protected $correctOptionId;	// The correct answer to this question.
-	protected $chosenOptionid;	// The answer that the user chose (if the question has been answered).
+	protected $text;		// Question text.
+	
+	//protected $correctOptionId;	// The correct answer to this question.
+	//protected $chosenOptionid;	// The answer that the user chose (if the question has been answered).
 
 	//Will behave differently based on Question type
 	// $makeQuestionText(){}
