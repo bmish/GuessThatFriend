5a1c888d3a09a6211dbdcce2e42b7b51de3eb5dc
Arjan Singh Nirh <arjanirh@gmail.com>
2012-03-11 23:38:31 -0500

    adding more test points

--- api/classes/FacebookTest.php
+++ api/classes/FacebookTest.php
@@ -18,21 +18,34 @@ class FacebookTest{
 		$arr_len = sizeof($subject_array);
 	
 		//Make sure the 'friends' json has more than 1 friend
-		echo '<p>Testing Array Length >0 '.($arr_len > 0).'</p>';
+		echo '<p> Testing NumFriends >0 : '.($arr_len > 0).'</p>';
 
-		/* Loop through all friends, check if friend name breaks code (eg hypenated names) */
+		/* Loop through all friends, check if friend information is valid */
 		echo 'Running string checks on Friend info...</p>';
 		for($i=0; $i<$arr_len; $i++){
 			$curr_subject = $subject_array[$i];
 
-			if(strlen($curr_subject->$facebookId)<=0)
+			/* Check if friend name is not blank */
+			if(strlen($curr_subject->$name)<=0)
 				echo '<p>array index='.$i.' has 0 length friend name</p>';
 
-			$result = strlen(strpos($curr_subject->$facebookId, "-"));
-			if($result != 0)
-				echo '<p> Failed sanity checks (hyphenated word) on '.$curr_subject->$facebookId.', array index='.$i.'</p>';
-
 			/* Check if facebookId is not blank */
+			if(strlen($curr_subject->$facebookId)<=0)
+				echo '<p>array index='.$i.' has 0 length facebook id</p>';
+
+			/* Check if Friend's display picture info is not corrupted */
+			if(strlen($curr_subject->$picture)<=0)
+				echo '<p>array index='.$i.' has corrupted picture information </p>';
+
+			/* Check if Friend's facebook link info is not corrupted */
+			if(strlen($curr_subject->$link)<=0)
+				echo '<p>array index='.$i.' has corrupted facebook link info</p>';
+
+			/* Loop through all friends, check if friend name breaks code (eg hypenated names) */
+			$result = strlen(strpos($curr_subject->$name, "-"));
+			if($result != 0)
+				echo '<p> Failed sanity checks (hyphenated word) on '.$curr_subject->$name.', array index='.$i.'</p>';
+
 		}
 		
 		
