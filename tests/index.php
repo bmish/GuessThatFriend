6c2e2f94799ea6893d8d49a88579868f5d25a5a4
Mike Robertson <mrobertson466@gmail.com>
2012-03-12 21:17:32 -0500

    renamed tests

--- tests/index.php
+++ tests/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once('simpletest/autorun.php');
 
-class TestOfTesting extends UnitTestCase {
+class APIIntegrationTests extends UnitTestCase {
 	function getQuizHelper($qCount, $oCount, $cat) {
 		$url = 'http://guessthatfriend.jasonsze.com/api/';
 		$addr=$url.'?cmd=getQuestions&facebookAccessToken=xxx&questionCount='.$qCount.'&optionCount='.$oCount.'&categoryId='.$cat;
@@ -32,71 +32,88 @@ class TestOfTesting extends UnitTestCase {
 		$this->assertEqual($json->questions[0], $questionId);
 	}
 
-	function testGetQuiz1() {
+        function testGetQuizOneQuestion() {
+                echo 'Questions=2,Options=2,Category=2<br><br>';
 		$this->getQuizHelper(1,2,2);
 	}
 
-	function testGetQuiz2() {
+        function testGetQuizFiveQuestions() {
+                echo 'Questions=5,Options=4,Category=3<br><br>';
 		$this->getQuizHelper(5,4,3);
 	}
 
-	function testGetQuiz3() {
+        function testGetQuizTenQuestions() {
+                echo 'Questions=10,Options=4,Category=3<br><br>';
 		$this->getQuizHelper(10,5,3);
 	}
 
-	function testGetQuiz4() {
+	function testGetQuizFifteenQuestions() {
+                echo 'Questions=15,Options=6,Category=3<br><br>';
 		$this->getQuizHelper(15,6,3);
 	}
 
-	function testGetQuiz5() {
+	function testGetQuizTwentyQuestions() {
+                echo 'Questions=20,Options=7,Category=3<br><br>';
 		$this->getQuizHelper(20,7,3);
 	}
 
-	function testGetQuiz6() {
+	function testGetQuizTwentyfiveQuestions() {
+                echo 'Questions=25,Options=8,Category=3<br><br>';
 		$this->getQuizHelper(25,8,3);
 	}
 
-	function testGetQuizLots() {
+	function testGetQuizLotsOfQuestions() {
+                echo 'Questions=250,Options=3,Category=3<br><br>';
 		$this->getQuizHelper(250,3,3);
 	}
 
 	function testGetQuizNegQCnt() {
+                echo 'Questions=-1,Options=3,Category=3<br><br>';
 		$this->getQuizHelper(-1,3,3);
 	}
 
 	function testGetQuizNegOpCnt() {
+                echo 'Questions=1,Options=-3,Category=3<br><br>';
 		$this->getQuizHelper(1,-3,3);
 	}
 
 	function testGetQuizNegCategory() {
+                echo 'Questions=1,Options=3,Category=-3<br><br>';
 		$this->getQuizHelper(1,3,-3);
 	}
 
 	function testGetQuizZeroOpt() {
+                echo 'Questions=1,Options=0,Category=3<br><br>';
 		$this->getQuizHelper(1,0,3);
 	}
 
 	function testGetQuizZeroQuestions() {
+                echo 'Questions=0,Options=3,Category=3<br><br>';
 		$this->getQuizHelper(0,3,3);
 	}
 
 	function testGetQuizStringQuestionNum() {
+                echo 'Questions="1",Options=2,Category=2<br><br>';
 		$this->getQuizHelper("1",2,2);
 	}
 
-	function testSubmitQuiz1() {
+        function testSubmitQuizQuestionIdEleven() {
+                echo 'QuestionID=11<br><br>';
 		$this->submitQuizHelper(11);
 	}
 
-	function testSubmitQuiz2() {
+	function testSubmitQuizQuestionIdTwelve() {
+                echo 'QuestionID=12<br><br>';
 		$this->submitQuizHelper(12);
 	}
 
-	function testSubmitQuiz3() {
+	function testSubmitQuizQuestionIdOneHundred() {
+                echo 'QuestionID=100<br><br>';
 		$this->submitQuizHelper(100);
 	}
 
 	function testSubmitQuizNegQuestion() {
+                echo 'QuestionID=-1<br><br>';
 		$this->submitQuizHelper(-1);
 	}
 }
