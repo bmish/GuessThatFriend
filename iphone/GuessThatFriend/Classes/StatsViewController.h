cc92eea4ffa43a57612e15a6491846cf16d1fb0b
Arjan Singh Nirh <arjanirh@mobile-99-98.near.uiuc.edu>
2012-04-09 22:47:34 -0500

    changes example json from 300Movie to IceAge movie so we could use the picture from graph.facebook.com

---
+++ iphone/GuessThatFriend/Classes/StatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  StatsViewController.h
+//  GuessThatFriend
+//
+//  Created by Arjan Singh Nirh on 4/9/12.
+//  Copyright (c) 2012 University of Illinois at Urbana-Champaign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatsViewController : UIViewController
+
+@end
---
+++ iphone/GuessThatFriend/Classes/StatsViewController.m
@@ -0,0 +1,44 @@
+//
+//  StatsViewController.m
+//  GuessThatFriend
+//
+//  Created by Arjan Singh Nirh on 4/9/12.
+//  Copyright (c) 2012 University of Illinois at Urbana-Champaign. All rights reserved.
+//
+
+#import "StatsViewController.h"
+
+@interface StatsViewController ()
+
+@end
+
+@implementation StatsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
