3ac0e158b3aac7f9e34f90ea428d4f44a32ce3f5
Tianyi Wang <wang103@illinois.edu>
2012-04-30 00:19:32 -0500

    fixed #44

--- iphone/GuessThatFriend/Classes/SettingsViewController.h
+++ iphone/GuessThatFriend/Classes/SettingsViewController.h
@@ -9,24 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface SettingsViewController : UIViewController {
-    UISegmentedControl *quizOptionSegmentedControl;
-    UILabel *quizOptionLabel;
-    UIButton *targetFriendEditButton;
-    UILabel *targetFriendLabel;
-    UIButton *categoryEditButton;
-    UILabel *categoryLabel;
 	UIButton *logoutButton;
 }
 
-@property (nonatomic, retain) IBOutlet UISegmentedControl *quizOptionSegmentedControl;
-@property (nonatomic, retain) IBOutlet UILabel *quizOptionLabel;
-@property (nonatomic, retain) IBOutlet UIButton *targetFriendEditButton;
-@property (nonatomic, retain) IBOutlet UILabel *targetFriendLabel;
-@property (nonatomic, retain) IBOutlet UIButton *categoryEditButton;
-@property (nonatomic, retain) IBOutlet UILabel *categoryLabel;
 @property (nonatomic, retain) IBOutlet UIButton *logoutButton;
 
 - (IBAction)switchViewToFBLogin:(id)sender;
-- (IBAction)quizOptionSegmentedControlChanged:(id)sender;
 
 @end
--- iphone/GuessThatFriend/Classes/SettingsViewController.m
+++ iphone/GuessThatFriend/Classes/SettingsViewController.m
@@ -13,12 +13,6 @@
 
 @implementation SettingsViewController
 
-@synthesize quizOptionSegmentedControl;
-@synthesize quizOptionLabel;
-@synthesize targetFriendEditButton;
-@synthesize targetFriendLabel;
-@synthesize categoryEditButton;
-@synthesize categoryLabel;
 @synthesize logoutButton;
 
 - (IBAction)backItemPressed:(id)sender {
@@ -55,66 +49,12 @@
 	UIImage *buttonImageNormal = [UIImage imageNamed:@"whiteButton.png"];
 	UIImage *stretchableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 topCapHeight:0];
 	[logoutButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
-	[targetFriendEditButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
-	[categoryEditButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
     
 	UIImage *buttonImagePressed = [UIImage imageNamed:@"blueButton.png"];
 	UIImage *stretchableButtonImagePressed = [buttonImagePressed stretchableImageWithLeftCapWidth:12 topCapHeight:0];
 	[logoutButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
-	[targetFriendEditButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
-	[categoryEditButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
-    
-    quizOptionSegmentedControl.selectedSegmentIndex = 4;
     
     [super viewDidLoad];
-}
-
-- (IBAction)quizOptionSegmentedControlChanged:(id)sender {
-    int selected = quizOptionSegmentedControl.selectedSegmentIndex;
-    QuizSettings *quizSettings = [QuizSettings quizSettingObject];
-    
-    NSString *option;
-    switch (selected) {
-            
-        /* From the API: 
-           -1: Random
-           0: Fill in the blank
-           2-6: Multiple choice
-        */
-        
-        case 0:
-            option = [NSString stringWithString:@"Random"];
-            quizSettings.option = -1;
-            break;
-        case 1:
-            option = [NSString stringWithString:@"Fill in the blank"];
-            quizSettings.option = 0;
-            break;
-        case 2:
-            option = [NSString stringWithString:@"2 choices"];
-            quizSettings.option = 2;
-            break;
-        case 3:
-            option = [NSString stringWithString:@"3 choices"];
-            quizSettings.option = 3;
-            break;
-        case 4:
-            option = [NSString stringWithString:@"4 choices"];
-            quizSettings.option = 4;
-            break;
-        case 5:
-            option = [NSString stringWithString:@"5 choices"];
-            quizSettings.option = 5;
-            break;
-        case 6:
-            option = [NSString stringWithString:@"6 choices"];
-            quizSettings.option = 6;
-            break;
-        default:
-            break;
-    }
-    
-    quizOptionLabel.text = option;
 }
 
 - (IBAction)switchViewToFBLogin:(id)sender {
@@ -133,21 +73,10 @@
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
     
-    self.quizOptionSegmentedControl = nil;
-    self.quizOptionLabel = nil;
-    self.targetFriendEditButton = nil;
-    self.targetFriendLabel = nil;
-    self.categoryEditButton = nil;
-    self.categoryLabel = nil;
+    self.logoutButton = nil;
 }
 
 - (void)dealloc {
-    [quizOptionSegmentedControl release];
-    [quizOptionLabel release];
-    [targetFriendEditButton release];
-    [targetFriendLabel release];
-    [categoryEditButton release];
-    [categoryLabel release];
     [logoutButton release];
 	
     [super dealloc];
