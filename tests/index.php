f796a3488c37fc8f5d6f40321ab70d668d525238
Ken Driscoll <driscoll.ken@gmail.com>
2012-05-02 14:37:28 -0500

    Added PHPDoc to the tests and added a few more tests
    The tests take awhile to run now…and they still require a user-passed
    Facebook token.

--- tests/index.php
+++ tests/index.php
@@ -3,6 +3,15 @@ require_once('simpletest/autorun.php');
 
 class APIIntegrationTests extends UnitTestCase {
 	var $fbtok;
+	
+	/**
+	 * getQuizHelper tests the getQuiz API call with passed arguments
+	 * @param integer $qCount the number of questions requested
+	 * @param integer $oCount the option number
+	 * @param integer $cat the category id for the questions
+	 * 
+	 * @return the last question number returned
+	 */
 	function getQuizHelper($qCount, $oCount, $cat) {
 		$this->fbtok = $_GET['facebookAccessToken'];
 		$url = 'http://guessthatfriend.jasonsze.com/api/';
@@ -16,8 +25,6 @@ class APIIntegrationTests extends UnitTestCase {
 		
 		$question = NULL;
 		foreach ($json->questions as $question) {
-			$category = $question->category->categoryId;
-			//$this->assertEqual(intval($category), $cat);
 			$opNum = sizeof($question->options);
 			$this->assertEqual($opNum, $oCount);
 		}
@@ -25,6 +32,11 @@ class APIIntegrationTests extends UnitTestCase {
 		return $question->questionId;
 	}
 
+	/**
+	 * submitQuizHelper tests the submitQuiz API call with passed argument
+	 * 
+	 * @param integer $questionId the question number we are answering.
+	 */
 	function submitQuizHelper($questionId) {
 		$this->fbtok = $_GET['facebookAccessToken'];
 		$url = 'http://guessthatfriend.jasonsze.com/api/';
@@ -39,40 +51,71 @@ class APIIntegrationTests extends UnitTestCase {
 		$this->assertEqual($json->questionIds[0], $questionId);
 	}
 
+	/**
+	 * testGetQuizOneQuestion tests getQuiz call with Questions=1,Options=2,Category=2
+	 */
         function testGetQuizOneQuestion() {
                 echo 'Questions=1,Options=2,Category=2<br><br>';
 		$this->getQuizHelper(1,2,2);
 	}
 
+	/**
+	* testGetQuizFiveQuestion tests getQuiz call with Questions=5,Options=4,Category=3
+	*/
         function testGetQuizFiveQuestions() {
                 echo 'Questions=5,Options=4,Category=3<br><br>';
 		$this->getQuizHelper(5,4,3);
 	}
 
+	/**
+	 * testGetQuizTenQuestions tests getQuiz call with Questions=10,Options=4,Category=3
+	 */
         function testGetQuizTenQuestions() {
                 echo 'Questions=10,Options=4,Category=3<br><br>';
 		$this->getQuizHelper(10,5,3);
 	}
 
+	/**
+	* testGetQuizFifteenQuestions tests getQuiz call with Questions=15,Options=6,Category=3
+	*/
 	function testGetQuizFifteenQuestions() {
                 echo 'Questions=15,Options=6,Category=3<br><br>';
 		$this->getQuizHelper(15,6,3);
 	}
 
+	/**
+	 * testSubmitQuizQuestion1 tests submitQuiz call with a single valid question number
+	 */
+	function testSubmitQuizQuestion1() {
+        $qID = intval($this->getQuizHelper(1,2,2));
+        echo 'QuestionID='.$qID.'<br><br>';
+		$this->submitQuizHelper($qID);
+	}
 	
-	function testSubmitQuizQuestionIdEleven() {
-                echo 'QuestionID=11<br><br>';
-		$this->submitQuizHelper(intval($this->getQuizHelper(1,2,2)));
+	/**
+	 * testSubmitQuizQuestionMultiple tests submitQuiz call with many valid question numbers
+	 */
+	function testSubmitQuizMultiple() {
+       $i=1;
+       while ($i<10){
+       		$qID = intval($this->getQuizHelper(1,2,2));
+        	echo 'QuestionID='.$qID.'<br><br>';
+			$this->submitQuizHelper($qID);
+			$i++;
+		}
 	}
-
-	function testSubmitQuizQuestionIdTwelve() {
-                echo 'QuestionID=12<br><br>';
-		$this->submitQuizHelper(intval($this->getQuizHelper(1,2,2)));
-	}
-
-	function testSubmitQuizQuestionIdOneHundred() {
-                echo 'QuestionID=100<br><br>';
-		$this->submitQuizHelper(intval($this->getQuizHelper(1,2,2)));
+	
+	/**
+	 * testSubmitQuizQuestionMultipleDiff tests submitQuiz call with many 
+	 */
+	function testSubmitQuizQuestionMultipleDiff() {
+       $i=1;
+       while ($i<10){
+       		$qID = intval($this->getQuizHelper($i,2,2));
+        	echo 'QuestionID='.$qID.'<br><br>';
+			$this->submitQuizHelper($qID);
+			$i++;
+		}
 	}
 	
 }
