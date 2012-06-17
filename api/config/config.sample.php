604ff9772a4c131ff3f9fcc222bb2ce14ac5fb3c
Bryan Mishkin <bryan.mishkin@gmail.com>
2012-06-17 16:28:46 -0700

    Added README and updated configuration documentation (fixes #102)

--- api/config/config.sample.php
+++ api/config/config.sample.php
@@ -1,14 +1,16 @@
 <?php
+// Make a copy of this file named config.php and fill in the variables.
+
 // Timezone:
 date_default_timezone_set('America/Chicago');
 
-// Database:
+// MySQL database connection:
 const DB_HOST = '127.0.0.1';
 const DB_USER = '';
 const DB_PASS = '';
 const DB_NAME = 'GuessThatFriend';
 
-// Facebook:
+// Facebook app information (must sign up to develop apps on Facebook and create an app):
 const FB_APP_ID = '';
 const FB_SECRET = '';
 ?>
