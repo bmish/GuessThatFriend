49c33bbabd2539d6df57390d9cf0e6be3dd22348
Bryan Mishkin <bryan.mishkin@gmail.com>
2012-06-17 21:54:04 -0700

    Commented out web documentation for unimplemented getQuestions fields

--- api/view/apiCommandTables/getQuestions.php
+++ api/view/apiCommandTables/getQuestions.php
@@ -26,11 +26,11 @@
 			<tr>
 				<td class="colParameter">optionCount</td>
 				<td class="colType fontCode">int</td>
-				<td class="colValue"><?php echo OptionType::RANDOM; ?>: Random<br /><?php echo OptionType::FILL_IN_THE_BLANK; ?>: Fill in the blank<br /><?php echo OptionType::MC_MIN; ?>-<?php echo OptionType::MC_MAX; ?>: Multiple choice</td>
+				<td class="colValue"><?php echo OptionType::RANDOM; ?>: Random<br /><!--<?php echo OptionType::FILL_IN_THE_BLANK; ?>: Fill in the blank<br />--><?php echo OptionType::MC_MIN; ?>-<?php echo OptionType::MC_MAX; ?>: Multiple choice</td>
 				<td class="colDefault">Default: <?php echo OptionType::DEFAULT_TYPE; ?></td>
 				<td>How many options (if any) should come with each question?</td>
 			</tr>
-			<tr>
+			<!--<tr>
 				<td class="colParameter">topicFacebookId</td>
 				<td class="colType fontCode">string</td>
 				<td class="colValue">Topic's Facebook Id</td>
@@ -43,7 +43,7 @@
 				<td class="colValue"><a href="?cmd=getCategories">Get category list</a></td>
 				<td class="colDefault">Default: All categories</td>
 				<td>Should the questions focus on a particular category of likes like books or movies?</td>
-			</tr>
+			</tr>-->
 		</table>
 		<div class="apiCommandSectionExample"><a href="?cmd=getQuestions&amp;facebookAccessToken=xxx&amp;questionCount=1&amp;optionCount=<?php echo OptionType::MC_2; ?>&amp;categoryId=2">?cmd=getQuestions&amp;facebookAccessToken=xxx&amp;questionCount=1&amp;optionCount=<?php echo OptionType::MC_2; ?>&amp;categoryId=2</a> - Hardcoded example.</div>
 	</div>
