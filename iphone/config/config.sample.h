97191672abeb143c49246d1572e228cfd05b4a30
Bryan Mishkin <bryan.mishkin@gmail.com>
2012-07-03 17:12:42 -0700

    Added the FACEBOOK_APP_ID and API_URL_ADDRESS of the original server to the README

--- iphone/config/config.sample.h
+++ iphone/config/config.sample.h
@@ -1,4 +1,4 @@
-#define FACEBOOK_APP_ID                         "xxx"
-#define API_URL_ADDRESS                         "http://xxx.com/api/"
-#define IMAGE_CACHE_FILE_COUNT_LIMIT            1000
-#define IMAGE_CACHE_AGE_LIMIT_SECONDS           60*60*24*7*2; // Two weeks.
+#define FACEBOOK_APP_ID                 "xxx"                                       // String that should be enclosed in quotes.
+#define API_URL_ADDRESS                 "http://xxx.com/api/"                       // Begins with 'http://' and ends with '/'
+#define IMAGE_CACHE_FILE_COUNT_LIMIT    1000
+#define IMAGE_CACHE_AGE_LIMIT_SECONDS   60*60*24*7*2;                               // Two weeks.
