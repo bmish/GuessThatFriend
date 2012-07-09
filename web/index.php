7e2b9b79a4a1232ae11390fb320fbf3e07cb7422
Bryan Mishkin <bryan.mishkin@gmail.com>
2012-07-09 13:02:56 -0700

    Added an app info and privacy policy website to be linked to from the app store (fixes #166 and fixes #168)

---
+++ web/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Guess That Friend</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<script src="js/fns.js"></script>
+</head>
+<body>
+<div id="wrapper">
+	<div id="header">
+		<div id="pageTitle">Guess That Friend</div>
+	</div>
+	<div id="page">
+		<div id="body">
+			<div id="youtubeVideoEmbed"><iframe width="560" height="315" src="http://www.youtube.com/embed/FrKhlnw2krM?rel=0" frameborder="0" allowfullscreen></iframe></div>
+			
+			<div class="section">
+				<div class="sectionHeader"><a id="about">About the app</a></div>
+				<div class="sectionBody">
+					<p>Guess That Friend is a trivia game based on the interests of your Facebook friends.</p>
+					<p>Answer questions about your friends in all the following categories:</p>
+					<ul>
+					<li>Athletes</li>
+					<li>Authors</li>
+					<li>Bands</li>
+					<li>Celebrities</li>
+					<li>Companies</li>
+					<li>Directors</li>
+					<li>Movies</li>
+					<li>Politicians</li>
+					<li>Products</li>
+					<li>Restaurants</li>
+					<li>Sports teams</li>
+					<li>TV shows</li>
+					<li>And more!</li>
+					</ul>
+					<p>And the more questions you answer, the better the game gets at figuring out which friends and categories you know best.</p>
+				</div>
+			</div>
+			
+			<div class="section">
+				<div class="sectionHeader"><a id="source">Source code</a></div>
+				<div class="sectionBody">
+					<p>The source code for the web API (back end) and iPhone game (front end) are both available on <a href="https://github.com/bmish/GuessThatFriend/">GitHub</a>.</p>
+				</div>
+			</div>
+			
+			<div class="section">
+				<div class="sectionHeader"><a id="privacy">Privacy</a></div>
+				<div class="sectionBody">
+					<p>All of your personal information is kept private. None of it is shared outside this app. We store only the following information:</p>
+					<ul>
+						<li>Your Facebook ID</li>
+						<li>The time of your first and last login</li>
+						<li>A cached list of your friends (for performance)</li>
+						<li>A cached list of each of your friends' liked pages (for performance)</li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+</body>
+</html>
